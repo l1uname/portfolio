a4df99a622826a617c28205e2aa82e07e5faf181
Emanuil Kostadinov <emanuil.kostadinov@siteground.com>
2023-09-28 10:10:59 +0300

    portfolio app 1.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# Portfolio - Emanuil Kostadinov
+
+This is a portfolio website built with HTML, CSS, and JavaScript, showcasing my work. It includes sections for an
+introduction, about me, projects, certifications, and a contact form.
+
+## Features
+
+- **Smooth Scrolling**
+    - Smooth navigation between different sections of the website.
+- **Dynamic Active Navbar**
+    - The active state in the navbar is dynamically updated based on the section currently in view.
+- **Project Redirects**
+    - Clicking on a project redirects to the respective project's URL.
+- **Certificate Redirects**
+    - Clicking on a certificate redirects to the respective certificate's URL.
+- **Social Icon Redirects**
+    - Clicking on a social icon redirects to the respective social media page.
+- **Google reCaptcha**
+    - The contact form uses Google reCaptcha for bot protection.
+
+## Usage
+
+To use this portfolio website, follow these steps:
+
+1. Clone the repository.
+2. Open the `index.html` file in a web browser.
---
+++ js/main.js
@@ -0,0 +1,126 @@
+const allSections = document.querySelectorAll('.section');
+const navbarList = document.querySelector('.navbar__list');
+const btnLearnMore = document.querySelector('.btn__learn-more');
+const navHeight = 100;
+
+// Handling cursor animation
+window.onload = function () {
+    setTimeout(function () {
+        const element = document.querySelector('.typing-animation');
+        element.style.animation = 'none';
+        element.style.borderRight = '.15em solid transparent';
+    }, 2500);
+};
+
+const revealSection = (entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+
+        // Revealing section
+        entry.target.classList.add('revealed');
+
+        // Get the href of the navbar link from the data-nav-link attribute
+        const navLinkHref = entry.target.dataset.navLink;
+
+        // Remove the active class from all navbar links
+        const navLinks = document.querySelectorAll('.navbar__list a');
+        navLinks.forEach(link => link.classList.remove('active'));
+
+        // Select the navbar link
+        const navLink = document.querySelector(`.navbar__list a[href="${navLinkHref}"]`);
+        navLink.classList.add('active');
+    });
+}
+
+document.querySelectorAll('.navbar__list a').forEach(link => {
+    link.classList.remove('active');
+});
+
+const observerSections = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.4,
+    rootMargin: `${navHeight}px`,
+})
+
+allSections.forEach(section => {
+    console.log(section);
+    observerSections.observe(section);
+    section.classList.remove('revealed');
+})
+
+// Handling smooth scrolling
+
+const handleSmoothScroll = (e, sectionSelector) => {
+    e.preventDefault();
+    const sectionId = document.querySelector(sectionSelector);
+    const sectionCoords = sectionId.getBoundingClientRect();
+    console.log('Revealing:', e.target);
+    window.scrollTo({
+        left: sectionCoords.left + window.pageXOffset,
+        top: sectionCoords.top + window.pageYOffset,
+        behavior: 'smooth'
+    });
+};
+
+// Event Listener for navbar links
+navbarList.addEventListener('click', function (e) {
+    if (e.target.tagName === 'A') {
+        const navLinkId = e.target.getAttribute('href').slice(1);
+        console.log(navLinkId);
+        handleSmoothScroll(e, `.section__${navLinkId}`);
+    }
+});
+
+// Event Listener for "Learn more" button
+btnLearnMore.addEventListener('click', function (e) {
+    handleSmoothScroll(e, '.section__about-me');
+});
+
+// Handle redirects
+function handleRedirect(elementClass, url) {
+    document.querySelector(`.${elementClass}`).addEventListener('click', function () {
+        window.open(url, '_blank');
+    });
+    document.querySelector(`.${elementClass}`).addEventListener('click', function (e) {
+        e.stopPropagation();
+    });
+}
+
+// Project redirects
+handleRedirect('project__gallery', 'https://gallery.wpdevelopment.org');
+handleRedirect('project__todo', 'https://notes.wpdevelopment.org');
+handleRedirect('project__weather', 'https://weather.wpdevelopment.org');
+
+// Certificate redirects
+handleRedirect('link__cert-basic', 'images/js-basics.pdf');
+handleRedirect('link__cert-fundamentals', 'images/js-fundamentals.pdf');
+handleRedirect('link__cert-advanced', 'images/js-advanced.pdf');
+
+// Social icon redirects
+handleRedirect('social__fb', 'https://www.facebook.com/emanuil.kostadinov.9');
+handleRedirect('social__x', 'https://x.com/emanuilko');
+handleRedirect('social__linkedin', 'https://linkedin.com/in/emanuil-kostadinov-330bba135');
+
+// GitHub redirects
+handleRedirect('link__gallery', 'https://github.com/l1uname');
+handleRedirect('link__notes', 'https://github.com/l1uname');
+handleRedirect('link__weather', 'https://github.com/l1uname');
+
+// Google reCaptcha
+document.getElementById('submitBtn').addEventListener('click', function (e) {
+    const response = grecaptcha.getResponse();
+    if (response.length === 0) {
+        e.preventDefault();
+        document.getElementById('recaptchaWarning').style.display = 'block';
+        document.getElementById('recaptchaBorder').classList.add('border-warning');
+    } else {
+        document.getElementById('recaptchaBorder').classList.remove('border-warning');
+        document.getElementById('submitBtn').disabled = false;
+    }
+});
+
+window.recaptchaCallback = function () {
+    document.getElementById('recaptchaWarning').style.display = 'none';
+    document.getElementById('recaptchaBorder').classList.remove('border-warning');
+    document.getElementById('submitBtn').disabled = false;
+}
